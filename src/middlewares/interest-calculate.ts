6fd96cf0dadee871c3a80abb79fa4aff49a882e1
TeerawatBeer <teerawat.n26@gmail.com>
2022-02-04 15:36:58 +0700

    withFixRate cal

--- src/middlewares/interest-calculate.ts
+++ src/middlewares/interest-calculate.ts
@@ -36,11 +36,12 @@ export const interestCalculate = async (req, res, next) => {
 const withFixRate = (startDate, endDate, rate, debtAmount) => {
   const sd = moment(startDate);
   const ed = moment(endDate);
-  // const days = ed.diff(sd, "days");
+  const days = ed.diff(sd, "days");
   // const months = ed.diff(sd, "months");
   const years = ed.diff(sd, "years", true);
   // console.log(days, months, years);
   // console.log(debtAmount * (rate / 100) * years);
-  const interest = Big(debtAmount).times(rate).div(100).times(years).toFixed(2)
+  // const interest = Big(debtAmount).times(rate).div(100).times(years).toFixed(2)
+  const interest = (debtAmount * ((days+1)/365) * (rate/100)).toFixed(2)
   return interest;
 };
